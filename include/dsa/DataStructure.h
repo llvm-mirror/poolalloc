6a4bc61084e45d0711b6659a39d6cbb55fa66e3f
John Criswell <criswell@uiuc.edu>
2007-12-07 21:05:23 +0000

    Removed deleteValue() and copyValue() from TDDataStructure; these methods should be implemented by the base class.
    git-svn-id: https://llvm.org/svn/llvm-project/poolalloc/trunk@44683 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/dsa/DataStructure.h
+++ include/dsa/DataStructure.h
@@ -272,13 +272,6 @@ public:
 
   virtual bool runOnModule(Module &M);
 
-#if 0
-  /// deleteValue/copyValue - Interfaces to update the DSGraphs in the program.
-  /// These correspond to the interfaces defined in the AliasAnalysis class.
-  void deleteValue(Value *V);
-  void copyValue(Value *From, Value *To);
-#endif
-
   /// print - Print out the analysis results...
   ///
   void print(std::ostream &O, const Module *M) const;
