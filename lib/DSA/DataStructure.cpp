e72e08474630fa6237ec38e73c9208736a99c2c8
John Criswell <criswell@uiuc.edu>
2009-05-18 15:08:28 +0000

    Updated to new LLVM API for TargetData. Patch contributed by Haohui Mai.
    git-svn-id: https://llvm.org/svn/llvm-project/poolalloc/trunk@72001 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/DSA/DataStructure.cpp
+++ lib/DSA/DataStructure.cpp
@@ -400,7 +400,7 @@ namespace {
           const ArrayType *AT = cast<ArrayType>(SS.Ty);
           ++SS.Idx;
           if (SS.Idx != AT->getNumElements()) {
-            SS.Offset += unsigned(TD.getTypePaddedSize(AT->getElementType()));
+            SS.Offset += unsigned(TD.getTypeAllocSize(AT->getElementType()));
             return;
           }
           Stack.pop_back();  // At the end of the array
@@ -435,7 +435,7 @@ namespace {
             assert(SS.Idx < AT->getNumElements());
             Stack.push_back(StackState(AT->getElementType(),
                                        SS.Offset+SS.Idx*
-                             unsigned(TD.getTypePaddedSize(AT->getElementType()))));
+                             unsigned(TD.getTypeAllocSize(AT->getElementType()))));
           }
         }
       }
@@ -492,7 +492,7 @@ bool DSNode::mergeTypeInfo(const Type *NewTy, unsigned Offset,
           (Size == 0 && !Ty->isSized() && !isArray()) ||
           (Size == 1 && Ty == Type::VoidTy && isArray()) ||
           (Size == 0 && !Ty->isSized() && !isArray()) ||
-          (TD.getTypePaddedSize(Ty) == Size)) &&
+          (TD.getTypeAllocSize(Ty) == Size)) &&
          "Size member of DSNode doesn't match the type structure!");
   assert(NewTy != Type::VoidTy && "Cannot merge void type into DSNode!");
 
@@ -515,7 +515,7 @@ bool DSNode::mergeTypeInfo(const Type *NewTy, unsigned Offset,
   }
 
   // Figure out how big the new type we're merging in is...
-  unsigned NewTySize = NewTy->isSized() ? (unsigned)TD.getTypePaddedSize(NewTy) : 0;
+  unsigned NewTySize = NewTy->isSized() ? (unsigned)TD.getTypeAllocSize(NewTy) : 0;
 
   // Otherwise check to see if we can fold this type into the current node.  If
   // we can't, we fold the node completely, if we can, we potentially update our
@@ -655,7 +655,7 @@ bool DSNode::mergeTypeInfo(const Type *NewTy, unsigned Offset,
   unsigned O = 0;
   const Type *SubType = Ty;
   while (O < Offset) {
-    assert(Offset-O < TD.getTypePaddedSize(SubType) && "Offset out of range!");
+    assert(Offset-O < TD.getTypeAllocSize(SubType) && "Offset out of range!");
 
     switch (SubType->getTypeID()) {
     case Type::StructTyID: {
@@ -670,7 +670,7 @@ bool DSNode::mergeTypeInfo(const Type *NewTy, unsigned Offset,
     }
     case Type::ArrayTyID: {
       SubType = cast<ArrayType>(SubType)->getElementType();
-      unsigned ElSize = (unsigned)TD.getTypePaddedSize(SubType);
+      unsigned ElSize = (unsigned)TD.getTypeAllocSize(SubType);
       unsigned Remainder = (Offset-O) % ElSize;
       O = Offset-Remainder;
       break;
@@ -692,7 +692,7 @@ bool DSNode::mergeTypeInfo(const Type *NewTy, unsigned Offset,
       isa<FunctionType>(NewTy)) return false;
 
   unsigned SubTypeSize = SubType->isSized() ?
-       (unsigned)TD.getTypePaddedSize(SubType) : 0;
+       (unsigned)TD.getTypeAllocSize(SubType) : 0;
 
   // Ok, we are getting desperate now.  Check for physical subtyping, where we
   // just require each element in the node to be compatible.
@@ -720,12 +720,12 @@ bool DSNode::mergeTypeInfo(const Type *NewTy, unsigned Offset,
       else
         NextPadSize = SubTypeSize;
       NextSubType = STy->getElementType(0);
-      NextSubTypeSize = (unsigned)TD.getTypePaddedSize(NextSubType);
+      NextSubTypeSize = (unsigned)TD.getTypeAllocSize(NextSubType);
       break;
     }
     case Type::ArrayTyID:
       NextSubType = cast<ArrayType>(SubType)->getElementType();
-      NextSubTypeSize = (unsigned)TD.getTypePaddedSize(NextSubType);
+      NextSubTypeSize = (unsigned)TD.getTypeAllocSize(NextSubType);
       NextPadSize = NextSubTypeSize;
       break;
     default: ;
