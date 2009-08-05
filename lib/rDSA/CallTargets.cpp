6e347db099555ca348fe007b66398d75799bcba2
Andrew Lenharth <andrewl@lenharth.org>
2009-08-05 00:30:22 +0000

    New branch to clean up DSA some, may focus on precision rather than speed too
    git-svn-id: https://llvm.org/svn/llvm-project/poolalloc/trunk@78134 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ lib/rDSA/CallTargets.cpp
@@ -0,0 +1,144 @@
+//=- lib/Analysis/IPA/CallTargets.cpp - Resolve Call Targets --*- C++ -*-=====//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by the LLVM research group and is distributed under
+// the University of Illinois Open Source License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This pass uses DSA to map targets of all calls, and reports on if it
+// thinks it knows all targets of a given call.
+//
+// Loop over all callsites, and lookup the DSNode for that site.  Pull the
+// Functions from the node as callees.
+// This is essentially a utility pass to simplify later passes that only depend
+// on call sites and callees to operate (such as a devirtualizer).
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Module.h"
+#include "llvm/Instructions.h"
+#include "rdsa/DataStructure.h"
+#include "rdsa/DSGraph.h"
+#include "rdsa/CallTargets.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/FormattedStream.h"
+#include "llvm/Constants.h"
+#include <ostream>
+using namespace llvm;
+
+namespace {
+  STATISTIC (DirCall, "Number of direct calls");
+  STATISTIC (IndCall, "Number of indirect calls");
+  STATISTIC (CompleteInd, "Number of complete indirect calls");
+  STATISTIC (CompleteEmpty, "Number of complete empty calls");
+
+  RegisterPass<CallTargetFinder> X("calltarget","Find Call Targets (uses DSA)");
+}
+
+char CallTargetFinder::ID;
+
+void CallTargetFinder::findIndTargets(Module &M)
+{
+  EQTDDataStructures* T = &getAnalysis<EQTDDataStructures>();
+  for (Module::iterator I = M.begin(), E = M.end(); I != E; ++I)
+    if (!I->isDeclaration())
+      for (Function::iterator F = I->begin(), FE = I->end(); F != FE; ++F)
+        for (BasicBlock::iterator B = F->begin(), BE = F->end(); B != BE; ++B)
+          if (isa<CallInst>(B) || isa<InvokeInst>(B)) {
+            CallSite cs = CallSite::get(B);
+            AllSites.push_back(cs);
+            Function* CF = cs.getCalledFunction();
+            // If the called function is casted from one function type to another, peer
+            // into the cast instruction and pull out the actual function being called.
+            if (ConstantExpr *CE = dyn_cast<ConstantExpr>(cs.getCalledValue()))
+              if (CE->getOpcode() == Instruction::BitCast &&
+                  isa<Function>(CE->getOperand(0)))
+                CF = cast<Function>(CE->getOperand(0));
+            
+            if (!CF) {
+              if (isa<ConstantPointerNull>(cs.getCalledValue())) {
+                ++DirCall;
+                CompleteSites.insert(cs);
+              } else {
+                IndCall++;
+                DSNode* N = T->getDSGraph(*cs.getCaller())
+                  ->getNodeForValue(cs.getCalledValue()).getNode();
+                assert (N && "CallTarget: findIndTargets: No DSNode!\n");
+                N->addFullFunctionList(IndMap[cs]);
+                if (N->isCompleteNode() && IndMap[cs].size()) {
+                  CompleteSites.insert(cs);
+                  ++CompleteInd;
+                } 
+                if (N->isCompleteNode() && !IndMap[cs].size()) {
+                  ++CompleteEmpty;
+                  DEBUG(errs() << "Call site empty: '"
+			<< cs.getInstruction()->getName() 
+			<< "' In '"
+			<< cs.getInstruction()->getParent()->getParent()->getName()
+			<< "'\n");
+                }
+              }
+            } else {
+              ++DirCall;
+              IndMap[cs].push_back(cs.getCalledFunction());
+              CompleteSites.insert(cs);
+            }
+          }
+}
+
+void CallTargetFinder::print(std::ostream &O, const Module *M) const
+{
+  O << "[* = incomplete] CS: func list\n";
+  for (std::map<CallSite, std::vector<const Function*> >::const_iterator ii =
+       IndMap.begin(),
+         ee = IndMap.end(); ii != ee; ++ii) {
+    if (!ii->first.getCalledFunction()) { //only print indirect
+      if (!isComplete(ii->first)) {
+        O << "* ";
+        CallSite cs = ii->first;
+        cs.getInstruction()->dump();
+        O << cs.getInstruction()->getParent()->getParent()->getNameStr() << " "
+          << cs.getInstruction()->getNameStr() << " ";
+      }
+      O << ii->first.getInstruction() << ":";
+      for (std::vector<const Function*>::const_iterator i = ii->second.begin(),
+             e = ii->second.end(); i != e; ++i) {
+        O << " " << (*i)->getNameStr();
+      }
+      O << "\n";
+    }
+  }
+}
+
+bool CallTargetFinder::runOnModule(Module &M) {
+  findIndTargets(M);
+  return false;
+}
+
+void CallTargetFinder::getAnalysisUsage(AnalysisUsage &AU) const {
+  AU.setPreservesAll();
+  AU.addRequired<EQTDDataStructures>();
+}
+
+std::vector<const Function*>::iterator CallTargetFinder::begin(CallSite cs) {
+  return IndMap[cs].begin();
+}
+
+std::vector<const Function*>::iterator CallTargetFinder::end(CallSite cs) {
+  return IndMap[cs].end();
+}
+
+bool CallTargetFinder::isComplete(CallSite cs) const {
+  return CompleteSites.find(cs) != CompleteSites.end();
+}
+
+std::list<CallSite>::iterator CallTargetFinder::cs_begin() {
+  return AllSites.begin();
+}
+
+std::list<CallSite>::iterator CallTargetFinder::cs_end() {
+  return AllSites.end();
+}
---
+++ lib/rDSA/CompleteBottomUp.cpp
@@ -0,0 +1,64 @@
+//===- CompleteBottomUp.cpp - Complete Bottom-Up Data Structure Graphs ----===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by the LLVM research group and is distributed under
+// the University of Illinois Open Source License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This is the exact same as the bottom-up graphs, but we use take a completed
+// call graph and inline all indirect callees into their callers graphs, making
+// the result more useful for things like pool allocation.
+//
+//===----------------------------------------------------------------------===//
+
+#define DEBUG_TYPE "dsa-cbu"
+#include "rdsa/DataStructure.h"
+#include "rdsa/DSGraph.h"
+#include "llvm/Module.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/Support/Debug.h"
+using namespace llvm;
+
+namespace {
+  RegisterPass<CompleteBUDataStructures>
+  X("dsa-cbu", "'Complete' Bottom-up Data Structure Analysis");
+}
+
+char CompleteBUDataStructures::ID;
+
+// run - Calculate the bottom up data structure graphs for each function in the
+// program.
+//
+bool CompleteBUDataStructures::runOnModule(Module &M) {
+  init(&getAnalysis<BUDataStructures>(), false, true, false, true);
+
+  buildIndirectFunctionSets(M);
+  formGlobalECs();
+
+  return runOnModuleInternal(M);
+}
+
+void CompleteBUDataStructures::buildIndirectFunctionSets(Module &M) {
+  // Loop over all of the indirect calls in the program.  If a call site can
+  // call multiple different functions, we need to unify all of the callees into
+  // the same equivalence class.
+  std::vector<const Instruction*> keys;
+  callee_get_keys(keys);
+
+  //mege nodes in the global graph for these functions
+  for (std::vector<const Instruction*>::iterator ii = keys.begin(), ee = keys.end();
+       ii != ee; ++ii) {
+    if (*ii) {
+      callee_iterator csi = callee_begin(*ii), cse = callee_end(*ii); 
+      if (csi != cse) ++csi;
+      DSGraph* G = getOrCreateGraph((*ii)->getParent()->getParent());
+      for ( ; csi != cse; ++csi) {
+        G->getNodeForValue(*csi).mergeWith(G->getNodeForValue((*ii)->getOperand(0)));
+        G->getNodeForValue((*ii)->getOperand(0)).getNode()->setGlobalMarker();
+        G->getNodeForValue((*ii)->getOperand(0)).getNode()->addGlobal(*csi);
+      }
+    }
+  }
+}
