7ae2970f5ced1d8bd6c8763e436742e0f353e1bf
Will Dietz <wdietz2@illinois.edu>
2010-07-08 20:08:05 +0000

    Added test case where DSA crashes when processing an indirect call to an external function.
    git-svn-id: https://llvm.org/svn/llvm-project/poolalloc/trunk@107901 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ test/dsa/regression/2010-07-08.FPDeclaration.c
@@ -0,0 +1,12 @@
+//Test that DSA doesn't crash when processing an indirect call to an external function.
+//RUN: llvm-gcc -S --emit-llvm -c %s -o %t.bc
+//RUN: dsaopt %t.bc -dsa-td -disable-output
+
+extern void func(void);
+
+int main() {
+  void (*fp)(void) = func;
+  fp();
+  return 0;
+}
+
