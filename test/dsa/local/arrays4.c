fc685f8305dd86670f896175306a848a2478e8cc
Arushi Aggarwal <aggarwa4@illinois.edu>
2010-09-22 02:03:09 +0000

    Test case for array of structs
    git-svn-id: https://llvm.org/svn/llvm-project/poolalloc/trunk@114524 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ test/dsa/local/arrays4.c
@@ -0,0 +1,26 @@
+//--Make sure we can run DSA on it! 
+//RUN: llvm-gcc %s -c --emit-llvm -o - |  \
+//RUN: dsaopt -dsa-bu -dsa-td -disable-output
+
+//H, S, G, R, M
+
+#include <stdlib.h>
+
+struct StructType {
+
+  int a;
+  int *b;
+};
+
+void func() {
+
+  struct StructType tmp[10];
+  int i;
+  for(i=0;i<10;i++) {
+    tmp[i].a = i;
+  }
+  
+  struct StructType s2 = tmp[0];
+  int * c = s2.b;
+}
+
