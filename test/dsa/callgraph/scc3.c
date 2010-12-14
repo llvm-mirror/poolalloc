9449702f857e1723e65bc612cd1a0e114aa0c47b
Arushi Aggarwal <aggarwa4@illinois.edu>
2010-12-14 01:50:09 +0000

    Simplified test case for infinite looping in BU. Happens in sqlite3.
    git-svn-id: https://llvm.org/svn/llvm-project/poolalloc/trunk@121744 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ test/dsa/callgraph/scc3.c
@@ -0,0 +1,40 @@
+// A, B, func are in an SCC
+// C is called indirectly from A/B
+// The indirect call site in C is resolved, once we process the SCC
+// But since, we never process the SCC graph again, 
+// we never inline func into the SCC graph(of A, B, and func) at that
+// call site.
+
+// when we inline the SCCGraph into D(due to call to A), the unresolved
+// call site is also inlined. As it is complete and can be resolved
+// we inline the SCC graph again, pulling in the unresolved call site
+// again. This causes an infinte looping in BU.
+
+;XFAIL:*
+;RUN: not
+
+typedef int* (*funcptr)(int *);
+
+static int* A();
+
+static int* func(int * arg) {
+  A();
+  return arg;
+}
+
+static int* C(funcptr f, int *arg) {
+  (*f)(arg);
+}
+
+static int *B() {
+  func(A());
+  return C(func, A());
+}
+
+static int* A() {
+  return func(B());
+}
+
+static int* D() {
+  return A();
+}
