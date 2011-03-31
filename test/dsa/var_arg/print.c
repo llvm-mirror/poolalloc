512c5f2e41cc9fb5f480b7cd90df754c20d2f976
Will Dietz <wdietz2@illinois.edu>
2011-03-31 07:58:42 +0000

    Fix test compilation errors, fix spurious failures due to printing pointer.
    git-svn-id: https://llvm.org/svn/llvm-project/poolalloc/trunk@128617 91177308-0d34-0410-b5e6-96231b3b80d8

--- test/dsa/var_arg/print.c
+++ test/dsa/var_arg/print.c
@@ -1,14 +1,14 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 //--build the code into a .bc
 //RUN: llvm-gcc -O0 %s -S --emit-llvm -o - | llvm-as > %t.bc
 //--check if ds-aa breaks, breaks opts, or results in miscompiled code
 //RUN: lli %t.bc > %t.refout
 //RUN: dsaopt %t.bc -ds-aa -O3 -o - | lli > %t.out
-//RUN: diff %t.refout %t.out
-//--check properties of this particular test
-//N/A
+// (Don't diff the results; this code prints a pointer which is silly to diff)
+//RUNNO: diff %t.refout %t.out
 
 void generic_sendmsg (const char *fmt, ...)
 {
@@ -20,9 +20,11 @@ void generic_sendmsg (const char *fmt, ...)
   printf("\n");
 }
 
-void main() {
+int main() {
   int *x = malloc(sizeof(int));
   generic_sendmsg("F %li %li %3.2f %3.2f", 1234, 1234,123.22, 123.45);
   generic_sendmsg("%s ID3:%s%s", "TEST", "AAA" ,  "Unknown");
   generic_sendmsg("%s ID3:%s%s %p", "TEST", "AAA" ,  "Unknown", x);
+
+  return 0;
 }
