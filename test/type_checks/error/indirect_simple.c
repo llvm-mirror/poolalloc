240281a65c0dd2da4586787bb89d6636891732f5
Arushi Aggarwal <aggarwa4@illinois.edu>
2011-06-02 17:19:32 +0000

    More testcases.
    git-svn-id: https://llvm.org/svn/llvm-project/poolalloc/trunk@132467 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ test/type_checks/error/indirect_simple.c
@@ -0,0 +1,31 @@
+/*
+ * Build into bitcode
+ * RUN: llvm-gcc -O0 %s --emit-llvm -c -o %t.bc
+ * RUN: adsaopt -internalize -mem2reg -typechecks %t.bc -o %t.tc.bc
+ * RUN: tc-link %t.tc.bc -o %t.tc1.bc
+ * RUN: llc %t.tc1.bc -o %t.tc1.s
+ * RUN: llvm-gcc %t.tc1.s -o %t.tc2
+ * Execute
+ * RUN: %t.tc2 >& %t.tc.out
+ */
+#include <stdio.h>
+
+typedef int* (*funcptr)(double *);
+
+static int *foo(double *d) {
+  return (int*)d;
+}
+static int *bar(double *d) {
+  return (int*)(d+1);
+}
+int main(int argc, char **argv)
+{
+
+ funcptr FP; 
+ FP = &foo;
+ if(argc > 5)
+   FP = &bar;
+ double d = 5.0;
+ int *t = (*FP)(&d);
+ int v = *t;
+}
---
+++ test/type_checks/error/misalign1.c
@@ -0,0 +1,34 @@
+/*
+ * Build into bitcode
+ * RUN: llvm-gcc -O0 %s --emit-llvm -c -o %t.bc
+ * RUN: adsaopt -internalize -mem2reg -typechecks %t.bc -o %t.tc.bc
+ * RUN: tc-link %t.tc.bc -o %t.tc1.bc
+ * RUN: llc %t.tc1.bc -o %t.tc1.s
+ * RUN: llvm-gcc %t.tc1.s -o %t.tc2
+ * Execute
+ * RUN: %t.tc2 >& %t.tc.out
+ * ;XFAIL:*
+ */
+
+#include<stdio.h>
+
+
+typedef unsigned char byte;    //!< byte type definition
+
+int testEndian()
+{
+  short s;
+  byte *p;
+
+  p=(byte*)&s;
+
+  *(p+1)=1;
+
+  return (s==0);
+}
+
+int main() {
+  testEndian();
+  return 0;
+}
+
