ffee3b238227b60f49160f5c1a8d7bf932334f31
Will Dietz <wdietz2@illinois.edu>
2011-08-12 08:50:39 +0000

    Pull in r137266, r137268, and r137281 from release_27 branch.
    git-svn-id: https://llvm.org/svn/llvm-project/poolalloc/trunk@137450 91177308-0d34-0410-b5e6-96231b3b80d8

--- test/type_checks/correct/libclamav_snprintf.c
+++ test/type_checks/correct/libclamav_snprintf.c
@@ -5,7 +5,7 @@
  * RUN: adsaopt -internalize -mem2reg -typechecks %t.bc -o %t.tc.bc
  * RUN: tc-link %t.tc.bc -o %t.tc1.bc
  * RUN: llc %t.tc1.bc -o %t.tc1.s
- * RUN: clang %t.tc1.s -o %t.tc2
+ * RUN: clang++ %t.tc1.s -o %t.tc2
  * Execute
  * RUN: %t.tc2 >& %t.tc.out
  */
--- test/type_checks/error/libclamav_snprintf1.c
+++ test/type_checks/error/libclamav_snprintf1.c
@@ -5,7 +5,7 @@
  * RUN: adsaopt -internalize -mem2reg -typechecks %t.bc -o %t.tc.bc
  * RUN: tc-link %t.tc.bc -o %t.tc1.bc
  * RUN: llc %t.tc1.bc -o %t.tc1.s
- * RUN: clang %t.tc1.s -o %t.tc2
+ * RUN: clang++ %t.tc1.s -o %t.tc2
  * Execute
  * RUN: %t.tc2 >& %t.tc.out
  * ;XFAIL:*
@@ -13,6 +13,8 @@
 
 #include <sys/types.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 
 static void
 dopr(char *buffer, size_t maxlen, const char *format, va_list args);
@@ -529,6 +531,7 @@ int mdprintf(const char *str, ...)
   return bytes;
 
 }
-void main () {
+int main () {
   mdprintf("%d %s %f", 5.0, "arushi", 4.0);
+  return 0;
 }
